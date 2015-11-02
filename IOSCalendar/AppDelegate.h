35d870bf0d6c59abc773f755f908620737f33828
Omer Abufara <o.m.h.a.b@hotmail.com>
2015-11-02 15:39:05 -0500

    Initial Commit

---
+++ IOSCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSCalendar
+//
+//  Created by Omer Abufara on 11/1/15.
+//  Copyright © 2015 Omer Abufara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSCalendar/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  IOSCalendar
+//
+//  Created by Omer Abufara on 11/1/15.
+//  Copyright © 2015 Omer Abufara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)nextAct:(id)sender;
+
+- (IBAction)prevAct:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *monthly;
+@property (weak, nonatomic) IBOutlet UILabel *year;
+@property (strong, nonatomic) NSDate *defaulComp;
+
+
+@end
+
