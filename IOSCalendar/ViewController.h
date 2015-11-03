0dee015d94d473e1fb4406223f88ae2233d6c9ad
Moran, Jennifer <moran014@macpc4.local>
2015-11-03 09:58:21 -0500

    Created CalendarViewController class and moved code there

--- IOSCalendar/ViewController.h
+++ IOSCalendar/ViewController.h
@@ -8,17 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
-
-- (IBAction)nextAct:(id)sender;
-
-- (IBAction)prevAct:(id)sender;
-
-@property (weak, nonatomic) IBOutlet UILabel *monthly;
-@property (weak, nonatomic) IBOutlet UILabel *year;
-@property (strong, nonatomic) NSDate *defaulComp;
-
-@property (strong, nonatomic) NSMutableArray *eventsArray;
+@interface ViewController : UIViewController 
 
 
 @end
