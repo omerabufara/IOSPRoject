32ef6dcf32f23069be6395dbc8700f15b3cf25d4
Abhinav Kapoor <ab_kap@abhinavs-macbook-air.local>
2015-11-09 11:58:39 -0500

    Edit Button on details page with segue

---
+++ IOSCalendar/EventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewController.h
+//  IOSCalendar
+//
+//  Created by Abhinav Kapoor on 11/9/15.
+//  Copyright © 2015 Omer Abufara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailViewController : UIViewController
+
+@end
