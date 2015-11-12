13555a9c2908ec3bf8aa109818c00f39b3b9c168
Omer Abufara <o.m.h.a.b@hotmail.com>
2015-11-12 14:42:53 -0500

    db

--- IOSCalendar/CalendarViewController.m
+++ IOSCalendar/CalendarViewController.m
@@ -45,7 +45,6 @@ NSInteger thisday;
     self.calendarTableView.dataSource = self;
     
     self.dbManager = [[DBManager alloc] initWithDatabaseFilename:@"IOSPSSH.sql"];
-    [self checkAndCreateDatabase];
     
     [self loadData];
 }
@@ -307,35 +306,6 @@ NSInteger thisday;
     [self loadData];
 }
 
--(void) checkAndCreateDatabase
-{
-    
-    NSArray *documentPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsDir = [documentPaths objectAtIndex:0];
-    NSString *databaseName = @"iOSPSSH.sql.SQLite";
-    NSString *databasePath = [documentsDir stringByAppendingPathComponent:databaseName];
-    // Check if the SQL database has already been saved to the users phone, if not then copy it over
-    BOOL success;
-    
-    // Create a FileManager object, we will use this to check the status
-    // of the database and to copy it over if required
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    // Check if the database has already been created in the users filesystem
-    success = [fileManager fileExistsAtPath:databasePath];
-    
-    // If the database already exists then return without doing anything
-    if(success) return;
-    
-    // If not then proceed to copy the database from the application to the users filesystem
-    
-    // Get the path to the database in the application package
-    NSString *databasePathFromApp = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:databaseName];
-    
-    // Copy the database from the package to the users filesystem
-    [fileManager copyItemAtPath:databasePathFromApp toPath:databasePath error:nil];
-    
-    
-}
 
 
 @end
